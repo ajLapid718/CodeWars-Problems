f471b61b423538212377bd4f73b53cf8a614894d
Allan James Lapid <ajLapid718@gmail.com>
2017-09-24 01:03:22 -0400

    Uh oh

--- 6kyu Kata/Find_The_Odd_Int.rb
+++
@@ -1,7 +0,0 @@
-# Given an array, find the int that appears an odd number of times.
-#
-# There will always be only one integer that appears an odd number of times.
-
-def find_it(seq)
-  seq.find { |num| num if seq.count(num) % 2 != 0 }
-end
--- 6kyu Kata/Micro_Optimization_Digit_Sum.rb
+++
@@ -1,7 +0,0 @@
-# You wrote a program that can calculate the sum of all the digits of a non-negative integer.
-#
-# However, it's not fast enough. Can you make it faster?
-
-def digit_sum(n)
-  n.to_s.chars.map(&:to_i).reduce(:+)
-end
