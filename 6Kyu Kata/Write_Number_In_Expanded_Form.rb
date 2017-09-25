d35b2801c0bf9556356d0f334e4e57c23c1f7023
Allan James Lapid <ajLapid718@gmail.com>
2017-09-24 21:46:19 -0400

    Numero 108

---
+++ 6Kyu Kata/Write_Number_In_Expanded_Form.rb
@@ -0,0 +1,24 @@
+# Write Number in Expanded Form
+#
+# You will be given a number and you will need to return it as a string in Expanded Form. For example:
+#
+# expanded_form(12); # Should return '10 + 2'
+# expanded_form(42); # Should return '40 + 2'
+# expanded_form(70304); # Should return '70000 + 300 + 4'
+# NOTE: All numbers will be whole numbers greater than 0.
+#
+# If you liked this kata, check out part 2!!
+
+def expanded_form(num)
+  digits_column = 1
+  expanded_version = []
+
+  num.to_s.reverse.chars.each do |digit|
+    if digit != "0"
+      expanded_version << digit.to_i * digits_column
+    end
+    digits_column *= 10
+  end
+
+  expanded_version.reverse.join(" + ")
+end
