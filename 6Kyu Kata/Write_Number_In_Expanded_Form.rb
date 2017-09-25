721858377d566012ec04a9c75b9b9972affc2760
Allan James Lapid <ajLapid718@gmail.com>
2017-09-24 22:28:49 -0400

    Small addition

--- 6Kyu Kata/Write_Number_In_Expanded_Form.rb
+++ 6Kyu Kata/Write_Number_In_Expanded_Form.rb
@@ -22,3 +22,34 @@ def expanded_form(num)
 
   expanded_version.reverse.join(" + ")
 end
+
+# Part II of the Kata is only available in JavaScript && Python, but here is my solution written in Ruby
+
+# Write Number in Expanded Form - Part 2
+#
+# This is version 2 of my 'Write Number in Exanded Form' Kata.
+#
+# You will be given a number and you will need to return it as a string in Expanded Form. For example:
+#
+# expandedForm(1.24); // should return '1 + 2/10 + 4/100'
+# expandedForm(7.304); // should return '7 + 3/10 + 4/1000'
+# expandedForm(0.04); // should return '4/100'
+
+def expandedForm(num)
+  digits_column = 1
+  expanded_version = []
+
+  num.to_s.chars.each_with_index do |digit, idx|
+  	next if digit == "."
+
+  	if idx == 0
+  		expanded_version << digit
+  	elsif digit != "0"
+      expanded_version << (digit) + "/#{digits_column}"
+    end
+
+    digits_column *= 10
+  end
+
+  expanded_version.tap { |op| op.shift if expanded_version[0] == "0" }.join(" + ")
+end
