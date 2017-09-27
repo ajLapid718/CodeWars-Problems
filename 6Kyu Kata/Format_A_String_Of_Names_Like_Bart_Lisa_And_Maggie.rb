e8312b69c92271b6f303dbf09e5c30afecd94a17
Allan James Lapid <ajLapid718@gmail.com>
2017-09-27 00:54:41 -0400

    Numero 122

---
+++ 6Kyu Kata/Format_A_String_Of_Names_Like_Bart_Lisa_And_Maggie.rb
@@ -0,0 +1,39 @@
+# Given: an array containing hashes of names
+#
+# Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+#
+# Example:
+#
+# list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+# # returns 'Bart, Lisa & Maggie'
+#
+# list([ {name: 'Bart'}, {name: 'Lisa'} ])
+# # returns 'Bart & Lisa'
+#
+# list([ {name: 'Bart'} ])
+# # returns 'Bart'
+#
+# list([])
+# # returns ''
+# Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
+
+def list(names)
+  return "" if names.empty?
+
+  people = []
+
+  names.each do |hsh|
+    people << hsh[:name]
+  end
+
+  case people.count
+  when 1
+    return people.first
+  when 2
+    return "#{people.first} & #{people.last}"
+  when 3
+    return "#{people.first}, #{people[1]} & #{people.last}"
+  else
+    return "#{people[0..people.count-2].join(", ")} & #{people.last}"
+  end
+end
