a4c9c817e205ad7c976e5475328be02df5fb628a
Allan James Lapid <ajLapid718@gmail.com>
2017-11-03 08:43:40 -0400

    Numero 171

--- 6Kyu Kata/Angle_Between_Clock_Hands.rb
+++ 6Kyu Kata/Angle_Between_Clock_Hands.rb
@@ -3,14 +3,12 @@
 # For example, at 3:00 the angle is π/2 (90 degrees)
 
 # Return the smaller of the angles ( <= π )
-
 # Return π if the hands are opposite
-
 # Note: The hour hand does not "snap" to the tick mark - e.g. at 6:30 the angle is not 0 because the hour hand is at 6.5, not 6.0
-
 # There is no "seconds" hand, so the minutes hand snaps to the minute
 
 # My Solution
+
 def hand_angle(date)
   hour = date.strftime("%H").to_f
   minute = date.strftime("%M").to_f
@@ -37,4 +35,16 @@ def hand_angle(date)
   deg_to_rad = (difference / 180) * (Math::PI)
 
   return deg_to_rad
+end
+
+# A solution from another Codewars user
+
+def hand_angle date
+  hr = date.strftime("%l").to_f
+  min = date.strftime("%M").to_f
+  hr_angle = 30 * hr + 0.5 * min  
+  min_angle = 6 * min
+
+  angle = [(hr_angle - min_angle).abs , (hr_angle - min_angle - 360).abs].min
+  angle/180 * 3.1416
 end
