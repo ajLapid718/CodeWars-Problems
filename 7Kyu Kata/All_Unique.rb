a3f6ee7a9f35d02eff99ab1ddf839b26c8a3278c
Allan James Lapid <ajLapid718@gmail.com>
2017-12-27 17:13:01 -0500

    Numero 174

---
+++ 7Kyu Kata/All_Unique.rb
@@ -0,0 +1,8 @@
+# Write a program to determine if a string contains all unique characters.
+# Return true if it does and false otherwise.
+
+# The string may contain any of the 128 ASCII characters.
+
+def unique_chars?(str)
+  str.chars == str.chars.uniq
+end
