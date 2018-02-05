dc149745304db365f88766dc7204c6fdc78749a5
Allan James Lapid <ajLapid718@gmail.com>
2018-02-05 11:25:42 -0500

    Numero 236

---
+++ 8Kyu Kata/simpleValidationOfAUsernameWithRegex.js
@@ -0,0 +1,18 @@
+/*
+
+Write a simple regex to validate a username.
+
+Allowed characters are:
+
+- lowercase letters
+- numbers
+- underscore
+
+Length shoould be between 4 and 16 characters.
+
+*/
+
+function validateUsr(username) {
+  res = /^[a-z0-9_]{4,16}$/.test(username);
+  return res;
+}
