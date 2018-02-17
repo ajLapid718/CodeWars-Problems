9fb2c8f7f1844ef1967258656db030c5294ff267
Allan James Lapid <ajLapid718@gmail.com>
2018-02-17 15:56:15 -0500

    Numero 269

---
+++ 6Kyu Kata/ShoppingList.js
@@ -0,0 +1,62 @@
+/*
+
+Description:
+Calculate the cost of a shopping list by writing a function. The function takes an argument which is an array consisting of sub-arrays. For example:
+
+shoppingListCost([["Orange Juice", 2],["Chocolate", 4],["Pears", 8]])
+The input array includes sub arrays containing the name and quantity of each item being purchased. Already existing in the global scope is an object which gives some information regarding the shopping items:
+
+var groceries = {
+  "Orange Juice": {
+      "price" : 1.5,
+      "discount": 10,
+      "bogof": false
+  },
+
+  "Chocolate": {
+      "price" : 2,
+      "discount" : 0,
+      "bogof": true
+  },
+
+  // more items...
+}
+This object contains the price of the item, the discount currently being offered on that item, and whether the item is currently "buy one get one free" (meaning for every item purchased, one of the same item is free).
+
+Return the cost of the shopping list. If the input array contains no items, return zero. Round the answer to 2 decimal places.
+
+All input arrays will be valid and contain items included in the global groceries object. Each item will only appear once in any input array.
+
+Examples
+
+shoppingListCost([["Orange Juice", 2],["Chocolate", 4]])
+returns 6.7
+
+shoppingListCost([["Chocolate", 5],["Orange Juice", 15]])
+returns 26.25
+
+*/
+
+function shoppingListCost(arr) {
+  var totalPrice = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    var currentItem = arr[i];
+    var nameOfFood = currentItem[0];
+    var quantityOfFood = currentItem[1];
+
+    var price = groceries[nameOfFood].price;
+    var discount = groceries[nameOfFood].discount;
+    var bogof = groceries[nameOfFood].bogof;
+
+    if (bogof === true) {
+      quantityOfFood = quantityOfFood / 2;
+    }
+
+    var subTotal = price * Math.round(quantityOfFood);
+
+    totalPrice = totalPrice + subTotal - (subTotal * discount/100);
+  }
+
+  return Math.round(totalPrice * 100) / 100;;
+}
