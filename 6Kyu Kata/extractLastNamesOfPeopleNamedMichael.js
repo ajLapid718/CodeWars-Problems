73be68fdfea85accfcab1b3257650de5cc28474c
Allan James Lapid <ajLapid718@gmail.com>
2018-02-17 17:14:06 -0500

    Numero 270

---
+++ 6Kyu Kata/extractLastNamesOfPeopleNamedMichael.js
@@ -0,0 +1,33 @@
+/*
+
+Description:
+Given a text, for example:
+
+const inputText = "Michael, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?";
+
+get an array of last names of people named Michael.
+
+The result should be:
+["Jordan", "Johnson", "Green", "Wood"]
+
+Notes:
+
+First name will always be Michael with upper case 'M'.
+There will always be a space character between 'Michael' and last name.
+Last name will always be one word, starting with an upper-case letter and continuing with lower-case letters.
+There will always be at least one 'Micheal' with a valid last name in the input text.
+
+*/
+
+function getMichaelLastName(inputText) {
+  var fullNames = inputText.match(/Michael\s[A-Z][a-z]{0,}/g);
+  var lastNames = [];
+
+  for (var i = 0; i < fullNames.length; i++) {
+    var currentFullName = fullNames[i];
+    var lastName = currentFullName.split(/Michael\s/)[1];
+    lastNames.push(lastName);
+  }
+
+  return lastNames;
+}
