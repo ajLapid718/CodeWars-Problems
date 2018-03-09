b0db6bd8f5f1d33d5ea77c2e2f5283d25a3c48cb
Allan James Lapid <ajLapid718@gmail.com>
2018-03-09 16:52:39 -0500

    Numero 328

---
+++ 8Kyu Kata/kataExampleTwist.js
@@ -0,0 +1,11 @@
+/*
+
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+var websites = []
+
+*/
+
+let websites = Array(1000).fill("codewars", 0, 1000);
