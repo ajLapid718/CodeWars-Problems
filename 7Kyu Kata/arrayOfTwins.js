4e70f72d80d51efbb0f7e151405fbb80787379f7
Allan James Lapid <ajLapid718@gmail.com>
2018-03-26 10:23:35 -0400

    Numero 347

---
+++ 7Kyu Kata/arrayOfTwins.js
@@ -0,0 +1,28 @@
+/*
+
+You are given an array of numbers.
+Check if the array contains of pairs, i.e every number occurs exactly two times.
+If true, return true, otherwise false.
+Your solution should not modify the input array.
+
+*/
+
+function twins(myArray) {
+  let obj = {};
+
+  for (let i = 0; i < myArray.length; i++) {
+    let num = myArray[i];
+    obj[num] = obj[num] + 1 || 1;
+  }
+
+  let frequencies = [];
+
+  for (let key in obj) {
+    let val = obj[key];
+    if (val !== 2) {
+      return false;
+    }
+  }
+
+  return true;
+}
