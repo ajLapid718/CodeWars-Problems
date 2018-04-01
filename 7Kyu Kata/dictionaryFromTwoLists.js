e836bd826956a75009076338c14f0bb056778757
Allan James Lapid <ajLapid718@gmail.com>
2018-04-01 18:11:57 -0400

    Numero 357

--- 7Kyu Kata/dictionaryFromTwoLists.js
+++ 7Kyu Kata/dictionaryFromTwoLists.js
@@ -3,7 +3,7 @@
 There are two lists of different length.
 The first one consists of keys, the second one consists of values.
 Write a function createDict(keys, values) that returns a dictionary created from keys and values.
-If there are not enough values, the rest of keys should have a None (JS null)value.
+If there are not enough values, the rest of keys should have a None (JS null) value.
 If there not enough keys, just ignore the rest of values.
 
 Example 1:
