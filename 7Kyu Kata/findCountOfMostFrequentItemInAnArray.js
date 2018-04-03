ce7fa67534ead1102e06df6349056c67597a2837
Allan James Lapid <ajLapid718@gmail.com>
2018-04-03 14:47:36 -0400

    Numero 361

---
+++ 7Kyu Kata/findCountOfMostFrequentItemInAnArray.js
@@ -0,0 +1,36 @@
+/*
+
+Complete the function to find the count of the most frequent item of an array.
+You can assume that input is an array of integers.
+For an empty array return 0.
+
+Example
+
+Input Array: [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3]
+Output: 5
+
+The most frequent number in the array is -1 and it occurs 5 times.
+
+*/
+
+function mostFrequentItemCount(collection) {
+  let obj = {};
+
+  for (let i = 0; i < collection.length; i++) {
+    let num = collection[i];
+    obj[num] = obj[num] + 1 || 1;
+  }
+
+  let firstItem = collection[0];
+  let maximum = obj[firstItem];
+
+  for (let key in obj) {
+    let val = obj[key];
+
+    if (val > maximum) {
+      maximum = val;
+    }
+  }
+
+  return parseInt(maximum) || 0;
+}
