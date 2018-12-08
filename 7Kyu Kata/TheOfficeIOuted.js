78d66803082dae079cb1a1dffa129fd29edf4afa
Allan James Lapid <ajLapid718@gmail.com>
2018-12-07 19:44:16 -0500

    Numero 528

---
+++ 7Kyu Kata/TheOfficeIOuted.js
@@ -0,0 +1,37 @@
+/*
+
+Your colleagues have been looking over you shoulder.
+When you should have been doing your boring real job, you've been using the work computers to smash in endless hours of codewars.
+
+In a team meeting, a terrible, awful person declares to the group that you aren't working.
+You're in trouble.
+You quickly have to gauge the feeling in the room to decide whether or not you should gather your things and leave.
+
+Given an object (meet) containing team member names as keys, and their happiness rating out of 10 as the value, you need to assess the overall happiness rating of the group.
+If <= 5, return 'Get Out Now!'.
+Else return 'Nice Work Champ!'.
+
+Happiness rating will be total score / number of people in the room.
+
+Note that your boss is in the room (boss), their score is worth double it's face value (but they are still just one person!).
+
+*/
+
+function outed(meet, boss) {
+  let total = 0;
+  let amountOfPeople = 0;
+
+  for (let key in meet) {
+    let val = meet[key];
+    if (key === boss) {
+      total += (val * 2);
+    }
+    else {
+      total += val;
+    }
+    amountOfPeople++;
+  }
+
+  let averageRating = total / amountOfPeople;
+  return averageRating <= 5 ? "Get Out Now!" : "Nice Work Champ!";
+}
