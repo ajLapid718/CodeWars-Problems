d786386bc45c0b26b413c6bb06a5beb09dded68a
Allan James Lapid <ajLapid718@gmail.com>
2019-01-11 10:43:53 -0500

    Numero 546

---
+++ 7Kyu Kata/MyLanguages.js
@@ -0,0 +1,25 @@
+/*
+
+Given a dictionary/hash/object of languages and your respective test results, return the list of languages where your test score is at least 60, in descending order of the results.
+
+Note: There will be no duplicate values.
+
+Examples
+{"Java": 10, "Ruby": 80, "Python": 65}  --> ["Ruby", "Python"]
+{"Hindi": 60, "Dutch" : 93, "Greek": 71} --> ["Dutch", "Greek", "Hindi"]
+{"C++": 50, "ASM": 10, "Haskell": 20}   --> []
+
+*/
+
+function myLanguages(results) {
+  let languages = [];
+
+  for (let language in results) {
+    let testScore = results[language];
+    if (testScore >= 60) {
+      languages.push(language);
+    }
+  }
+
+  return languages.sort((a,b) => results[b] - results[a]);
+}
